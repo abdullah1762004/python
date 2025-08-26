dc113f65045236b648bc121448fb7619693e1a0f
Abdullah Shahid <a.shahid1762004@gmail.com>
2025-08-27 03:01:34 +1000

    Variables

---
+++ variable.py
@@ -0,0 +1,6 @@
+Name = input("Enter your Name:")
+num_1 = float(input("Enter Value Of Number 1:"))
+num_2 = float(input("Enter Value Of Number 2:")) 
+sum = num_1+num_2
+print("Solved By Name")
+print("Sum Of Num_1 And Num_2 : ",sum)
