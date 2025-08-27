29b2d30ab7ca16e7f05251bcd00aede9960d5574
Abdullah Shahid <a.shahid1762004@gmail.com>
2025-08-28 04:40:22 +1000

    expression code in python

---
+++ expression.py
@@ -0,0 +1,6 @@
+first_name = input("Enter First Nmae :")
+last_name = input("Enter Last Nmae :")
+space=" "
+full_name = first_name+space+last_name
+print("Full Name :",full_name)
+print(type(full_name))
