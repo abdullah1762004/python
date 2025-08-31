d9a9dd83349e41337c6c88c50ec912146af2c917
Abdullah Shahid <a.shahid1762004@gmail.com>
2025-09-01 06:09:52 +1000

    Code For Expression

--- expression.py
+++ expression.py
@@ -1,6 +1,19 @@
+#adding string
 first_name = input("Enter First Nmae :")
-last_name = input("Enter Last Nmae :")
+last_name = input("Enter Last Name :")
 space=" "
 full_name = first_name+space+last_name
 print("Full Name :",full_name)
 print(type(full_name))
+
+#sumof two numbers
+a=float(input("Enter value of a:"))
+b=float(input("Enter value of b:"))
+sum=a+b
+print("Sum of a and b",sum)
+
+#Bmi calculator 
+weight=float(input("Enter your weight in kg:"))
+height=float(input("Enter your height in cm:"))
+bmi=weight/((height/100)**2)
+print ("your Bmi is :",bmi)
